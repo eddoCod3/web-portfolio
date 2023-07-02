de10cbf912fe014b5be6e7e16c7398c2ec8eb03c
Eduardo <eddo.code.dev@gmail.com>
2023-07-02 00:35:39 -0600

    Adding files

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// function interpolateUserText() {
+//   const userPhrase = ["Edd", "I like to code"];
+//   let userText = document.getElementById("user_text");
+
+//   setInterval(function () {
+//     for (let i = 0; i < userPhrase.length; i++) {
+//       userText.innerHTML += userPhrase[i];
+//     }
+//   }, 1000);
+// }
+
